bd5dccc5589fe25920b94380e78b707da156f09c
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-12-31 19:55:12 -0600

    create custom middleware validateFields

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,15 +4,6 @@ const { validationResult } = require( 'express-validator' );
 const createUser = ( req, res = response ) => {
     const { name, email, password } = req.body;
 
-    // Todo: manejo de errores
-    const errors = validationResult( req );
-    if( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
-
     res.status(201).json({
         ok: true,
         msg: 'register',
@@ -23,15 +14,6 @@ const createUser = ( req, res = response ) => {
 }
 
 const loginUser = ( req, res = response ) => {
-     // Todo: manejo de errores
-    const errors = validationResult( req );
-    if( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
-    
     res.status(201).json({
         ok: true,
         msg: 'login'
--- routers/auth.js
+++ routers/auth.js
@@ -7,12 +7,14 @@ const { check } = require( 'express-validator' );
 const router = Router();
 
 const { createUser, loginUser, renewToken } = require( '../controllers/auth' );
+const { validateFields } = require('../middlewares/validateFields');
 
 router.post( '/register', 
     [
         check( 'name', 'El nombre es obligatorio' ).not().isEmpty(),
         check( 'email', 'El Email es obligatorio' ).isEmail(),
-        check( 'password', 'El password debe de ser de minimo de 6 caracteres' ).isLength({ min: 6 })
+        check( 'password', 'El password debe de ser de minimo de 6 caracteres' ).isLength({ min: 6 }),
+        validateFields
     ], 
     createUser 
 );
@@ -20,7 +22,8 @@ router.post( '/register',
 router.post( '/login', 
     [
         check( 'email', 'El Email es obligatorio' ).isEmail(),
-        check( 'password', 'El passowd debe de ser de minimo de 6 caracteres' ).isLength({ min: 6 })
+        check( 'password', 'El passowd debe de ser de minimo de 6 caracteres' ).isLength({ min: 6 }),
+        validateFields
     ],
     loginUser 
 );
