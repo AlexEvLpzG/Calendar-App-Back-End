593dd3116edf95e5ca49089b5e3a0220845ac782
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-12-31 20:59:09 -0600

    add the function to create user to the DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,40 @@
 const { response } = require( 'express' );
-const { validationResult } = require( 'express-validator' );
+const bcryptjs = require( 'bcryptjs' );
+const User = require( '../models/User' );
 
-const createUser = ( req, res = response ) => {
+const createUser = async( req, res = response ) => {
     const { name, email, password } = req.body;
 
-    res.status(201).json({
-        ok: true,
-        msg: 'register',
-        name,
-        email,
-        password
-    });
+    try {
+        let user = await User.findOne({ email });
+
+        if( user ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ya existe un usuario con la misma direccion E-mail'
+            });
+        }
+
+        user = new User( req.body );
+
+        // Todo: Encriptar contraseña
+        const salt = bcryptjs.genSaltSync();
+        user.password = bcryptjs.hashSync( password, salt );
+
+        await user.save();
+
+        res.status(201).json({
+            ok: true,
+            uuid: user.id,
+            name: user.name
+        });
+    } catch (error) {
+        console.log( error );
+        res.status(500).json({
+            ok: true,
+            msg: 'Por fabor hable con el administrador'
+        });
+    }
 }
 
 const loginUser = ( req, res = response ) => {
